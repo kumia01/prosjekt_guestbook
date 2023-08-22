2a8bf29b7e7808405c6b25d66fcff5c7d6725994
kumia01 <thien11hei@gmail.com>
2023-08-22 16:11:41 +0200

    lagt til vpc og subnet

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,23 @@
-resource "aws_ssm_parameter" "foo" {
-    name = "foo"
-    type = "String"
-    value = "bar"
+provider "aws" {
+    region = var.availability_zone
 }
+
+
+resource "aws_vpc" "application_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "App_vpc"
+  }
+}
+
+resource "aws_subnet" "priv_sub" {
+  vpc_id = aws_vpc.application_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = var.availability_zone
+  tags = {
+    Name = "Application_subnet_priv"
+  }
+}
+
+
