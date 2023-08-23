ea407b1ff8c6a831ba1c163411bcb9d54eeb6e6c
kumia01 <thien11hei@gmail.com>
2023-08-23 11:13:22 +0200

    bug fixing for eks

--- infra/main.tf
+++ infra/main.tf
@@ -1,12 +1,16 @@
 
 module "eks" {
     source = "terraform-aws-modules/eks/aws"
-    cluster_name = "my_cluster"
-    cluster_version = "1.21"
-    subnets = [aws_subnet.priv_sub_a.id, aws_subnet.priv_sub_b.id]
+    version = "~> 19.0"
+
+    cluster_name = "application_cluster"
+    cluster_version = "1.27"
+
+    vpc_id = aws_vpc.application_vpc.id
+    subnet_ids = [aws_subnet.priv_sub_a.id, aws_subnet.priv_sub_b.id]
 
     node_groups = {
-        eks_nodes= {
+        eks_nodes = {
             desired_capacity = 1
             max_capacity = 2
             min_capacity = 1
