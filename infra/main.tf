e3c9ed4c33ff1b6bd71838ee2c291f950e3531aa
kumia01 <thien11hei@gmail.com>
2023-08-23 11:18:39 +0200

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -8,19 +8,19 @@ module "eks" {
 
     vpc_id = aws_vpc.application_vpc.id
     subnet_ids = [aws_subnet.priv_sub_a.id, aws_subnet.priv_sub_b.id]
+}
 
-    eks_managed_node_gorups_defaults = {
+eks_managed_node_gorups_defaults = {
         instance_types = ["t2.micro"]
-    }
+}
 
-    eks_managed_node_groups = {
-        worker_node = {
-            min_size = 1
-            max_size = 2
-            desired_size = 1
-
-            instance_type = ["t2.micro"]
-        }
+eks_managed_node_groups = {
+    worker_node = {
+        min_size = 1
+        max_size = 2
+        desired_size = 1
+        
+        instance_type = ["t2.micro"]
     }
 }
 
