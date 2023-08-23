7caee7f102528902c4636209d0490a1e02e34ac0
kumia01 <thien11hei@gmail.com>
2023-08-23 15:37:30 +0200

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -13,32 +13,6 @@ resource "aws_eks_cluster" "application_cluster" {
     depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
 }
 
-resource "aws_launch_template" "eks_worker" {
-
-  name_prefix   = "worker"
-  image_id      = "ami-07bfff1c04cd9d7bd" # Your AMI ID
-  instance_type = "m5.large"
-
-}
-
-resource "aws_autoscaling_group" "eks_worker_group"{
-    min_size = 1
-    max_size = 2
-    desired_capacity = 2
-    vpc_zone_identifier = [aws_subnet.priv_sub_a.id, aws_subnet.priv_sub_b.id]
-    
-    launch_template {
-      id = aws_launch_template.eks_worker.id
-      version = "$Latest"
-    }
-
-    tag {
-        key = "Name"
-        value = "eks-worker-node"
-        propagate_at_launch = true
-    }
-}
-
 resource "aws_eks_node_group" "worker_nodes" {
     cluster_name = aws_eks_cluster.applications_cluster.name
     node_group_name = "my-worker-group"
